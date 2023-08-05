65c5ff898306a1434320575a0e3b1b90d027b085
codetective <iaminiodu@gmail.com>
2023-08-05 09:52:15 +0100

    added 404 page css

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,11 +2,15 @@ import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <div className='flex items-center justify-center text-center text-lg'>
-      <h1>Oops! You seem to be lost.</h1>
+    <div className='flex h-screen flex-col items-center justify-center text-center text-lg'>
+      <h1 className='text-3xl'>Oops! You seem to be lost.</h1>
       <p>Here are some helpful links:</p>
-      <Link to='/'>Home</Link>
-      <Link to='/apply'>Apply</Link>
+      <Link className='font-bold hover:text-blue-500 py-5' to='/'>
+        Home
+      </Link>
+      <Link className='font-bold hover:text-blue-500 py-5' to='/apply'>
+        Apply
+      </Link>
     </div>
   );
 }
